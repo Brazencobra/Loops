353dc87fb8023d7d7a81b1c58a7f170dd0cf2bc3
Brazencobra <elxan.memmedli2003@gmail.com>
2022-11-01 00:12:38 +0400

    1ci taski hec cure ede bilmedim :(

---
+++ Loops/Loops/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task1
+
+            // bu tapsirigi ede bilmedim :(
+
+            #endregion
+
+            #region Task2
+            //int number, result = 0;
+            //Console.Write("Ededi daxil edin:");
+            //number = Convert.ToInt32(Console.ReadLine());
+
+            //if (number > 0)
+            //{
+
+            //    while (number > 0)
+            //    {
+            //        result += number % 10;
+            //        number = number / 10;
+            //    }
+            //    Console.WriteLine("Daxil edilen ededin reqemleri cemi:" + result);
+
+            //}
+            #endregion
+
+            #region Task3
+            //int number;
+
+            //Console.Write("Ededi daxil edin:");
+            //number = Convert.ToInt32(Console.ReadLine());
+            //if (number == 0 || number == 1)
+            //{
+            //    Console.WriteLine(number + " murekkeb ededdir");
+            //    Console.ReadLine();
+            //}
+            //else
+            //{
+            //    for (int i = 2; i <= number / 2; i++)
+            //    {
+            //        if (number % i == 0)
+            //        {
+            //            Console.WriteLine(number + " murekkeb ededdir");
+            //            return;
+            //        }
+
+            //    }
+            //    Console.WriteLine(number + " sade ededdir");
+            //    Console.ReadLine();
+            //}
+            #endregion
+
+        }
+    }
+}
